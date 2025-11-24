920083f40642c6506490735273f7e448035c8ae0
Anshuma Singh <anshumansingh9807@gmail.com>
2025-11-25 02:38:22 +0530

    Add marketplace app and implement vendor listing

--- accounts/views.py
+++ accounts/views.py
@@ -12,6 +12,9 @@ from django.contrib.auth.tokens import default_token_generator
 
 from vendor.models import Vendor
 
+# from django.utils.text import slugify
+from django.template.defaultfilters import slugify
+
 # Create your views here.
 
 
@@ -93,6 +96,8 @@ def registerVendor(request):
             user.save()
             vendor = v_form.save(commit=False)
             vendor.user = user
+            vendor_name = v_form.cleaned_data['vendor_name']
+            vendor.vendor_slug = slugify(vendor_name)+'-'+str(user.id)
             user_profile = UserProfile.objects.get(user=user)
             vendor.user_profile = user_profile
             vendor.save()
--- foodOnline_main/urls.py
+++ foodOnline_main/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home, name='home'),
     path('',include('accounts.urls')),
+    path('marketplace/',include('marketplace.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
